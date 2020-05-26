b738b26fe02b72377eb3de827ad962285e19b7f7
Shade-Dev <38840823+shade-sdev@users.noreply.github.com>
2020-05-26 21:43:52 +0400

    Add files via upload

---
+++ RssNotifyer/RssNotifyer.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RssNotifyer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RssNotifyer))
+        Me.GunaElipse1 = New Guna.UI.WinForms.GunaElipse(Me.components)
+        Me.ContentFlowPanel = New System.Windows.Forms.FlowLayoutPanel()
+        Me.GunaDragControl1 = New Guna.UI.WinForms.GunaDragControl(Me.components)
+        Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
+        Me.GunaControlBox1 = New Guna.UI.WinForms.GunaControlBox()
+        Me.GunaControlBox2 = New Guna.UI.WinForms.GunaControlBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GunaElipse1
+        '
+        Me.GunaElipse1.TargetControl = Me
+        '
+        'ContentFlowPanel
+        '
+        Me.ContentFlowPanel.AutoSize = True
+        Me.ContentFlowPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.ContentFlowPanel.Location = New System.Drawing.Point(0, 51)
+        Me.ContentFlowPanel.Name = "ContentFlowPanel"
+        Me.ContentFlowPanel.Size = New System.Drawing.Size(799, 446)
+        Me.ContentFlowPanel.TabIndex = 0
+        '
+        'GunaDragControl1
+        '
+        Me.GunaDragControl1.TargetControl = Me
+        '
+        'FileSystemWatcher1
+        '
+        Me.FileSystemWatcher1.EnableRaisingEvents = True
+        Me.FileSystemWatcher1.SynchronizingObject = Me
+        '
+        'GunaControlBox1
+        '
+        Me.GunaControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox1.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox1.AnimationSpeed = 0.03!
+        Me.GunaControlBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaControlBox1.IconColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaControlBox1.IconSize = 15.0!
+        Me.GunaControlBox1.Location = New System.Drawing.Point(754, 0)
+        Me.GunaControlBox1.Name = "GunaControlBox1"
+        Me.GunaControlBox1.OnHoverBackColor = System.Drawing.Color.DarkRed
+        Me.GunaControlBox1.OnHoverIconColor = System.Drawing.Color.DarkRed
+        Me.GunaControlBox1.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox1.Size = New System.Drawing.Size(45, 29)
+        Me.GunaControlBox1.TabIndex = 1
+        '
+        'GunaControlBox2
+        '
+        Me.GunaControlBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GunaControlBox2.AnimationHoverSpeed = 0.07!
+        Me.GunaControlBox2.AnimationSpeed = 0.03!
+        Me.GunaControlBox2.BackColor = System.Drawing.Color.Transparent
+        Me.GunaControlBox2.ControlBoxType = Guna.UI.WinForms.FormControlBoxType.MinimizeBox
+        Me.GunaControlBox2.IconColor = System.Drawing.Color.WhiteSmoke
+        Me.GunaControlBox2.IconSize = 15.0!
+        Me.GunaControlBox2.Location = New System.Drawing.Point(709, 0)
+        Me.GunaControlBox2.Name = "GunaControlBox2"
+        Me.GunaControlBox2.OnHoverBackColor = System.Drawing.Color.DarkRed
+        Me.GunaControlBox2.OnHoverIconColor = System.Drawing.Color.Maroon
+        Me.GunaControlBox2.OnPressedColor = System.Drawing.Color.Black
+        Me.GunaControlBox2.Size = New System.Drawing.Size(45, 29)
+        Me.GunaControlBox2.TabIndex = 2
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 60000
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "RedditNotifyer"
+        Me.NotifyIcon1.Visible = True
+        '
+        'RssNotifyer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 500)
+        Me.Controls.Add(Me.GunaControlBox2)
+        Me.Controls.Add(Me.GunaControlBox1)
+        Me.Controls.Add(Me.ContentFlowPanel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "RssNotifyer"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GunaElipse1 As Guna.UI.WinForms.GunaElipse
+    Friend WithEvents ContentFlowPanel As FlowLayoutPanel
+    Friend WithEvents GunaDragControl1 As Guna.UI.WinForms.GunaDragControl
+    Friend WithEvents FileSystemWatcher1 As IO.FileSystemWatcher
+    Friend WithEvents GunaControlBox1 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents GunaControlBox2 As Guna.UI.WinForms.GunaControlBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ RssNotifyer/RssNotifyer.vb
@@ -0,0 +1,222 @@
+﻿Imports System.Text
+Imports System.Xml
+Imports System.ServiceModel.Syndication
+Imports System.IO
+Imports System.Reflection
+Imports Guna.UI.WinForms
+Imports Bunifu.UI.WinForms
+
+Public Class RssNotifyer
+    Dim count As Integer = 0
+    Dim FirstTitle As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        GetRss()
+
+    End Sub
+
+    Public Sub GetRss()
+
+        CheckStartUp()
+        ' SetLauncherPathSetting("Config.ini", 0, "username=")
+
+        Dim url As String
+        If GetLauncherPath("Config.ini", 1, 13).LastIndexOf("/") <> -1 Then
+            url = GetLauncherPath("Config.ini", 1, 13) & ".rss"
+        ElseIf GetLauncherPath("Config.ini", 1, 13).LastIndexOf("/") <> 1 Then
+            url = GetLauncherPath("Config.ini", 1, 13) & "/.rss"
+
+        End If
+
+
+        Try
+
+
+            Dim reader As XmlReader = XmlReader.Create(url)
+
+            Dim feed As SyndicationFeed = SyndicationFeed.Load(reader)
+            reader.Close()
+
+            For Each item As SyndicationItem In feed.Items
+
+                count = count + 1
+                'Debug.WriteLine(item.Title.Text) 'title
+                'Debug.WriteLine(item.Links.First.Uri.ToString()) 'reddit link
+                'Debug.WriteLine(item.Authors.First.Name.ToString()) 'user
+                'Debug.WriteLine(item.LastUpdatedTime.ToString()) 'time updated
+
+                Dim ContentPanel As GunaElipsePanel
+                ContentPanel = New Guna.UI.WinForms.GunaElipsePanel()
+
+                Dim Title As Label
+                Title = New Label()
+
+                Dim User As Label
+                User = New Label()
+
+                Dim Time As Label
+                Time = New Label()
+
+
+                'Set panel properties
+                With ContentPanel
+
+                    .Margin = New Padding(25, 20, 0, 0)
+                    .Size = New Size(750, 100)
+                    .Name = item.Title.Text
+                    .BaseColor = System.Drawing.Color.FromArgb(23, 23, 23)
+                    .Radius = 6
+                    AddHandler ContentPanel.Click, Sub(s As Object, e As EventArgs)
+                                                       Process.Start(item.Links.First.Uri.ToString())
+                                                   End Sub
+                End With
+
+                'Set Titlelabel Properties
+                With Title
+                    .Top = ((100 - (Title.Height)) / 2)
+                    .TextAlign = ContentAlignment.MiddleCenter
+                    .Size = New Size(750, 20)
+                    .Font = New System.Drawing.Font("Segoe UI SemiBold", 11.0!)
+                    .Name = "title" & count
+                    .Text = item.Title.Text
+                    .ForeColor = System.Drawing.Color.FromArgb(255, 255, 255)
+
+
+                    AddHandler Title.Click, Sub(s As Object, e As EventArgs)
+                                                Process.Start(item.Links.First.Uri.ToString())
+                                            End Sub
+                End With
+
+                'Set Userlabel Properties
+                With User
+                    .Top = 86
+                    .TextAlign = ContentAlignment.BottomRight
+                    .Size = New Size(750, 9)
+                    .Font = New System.Drawing.Font("Segoe UI", 6.0!)
+                    .Name = item.Authors.First.Name.ToString()
+                    .Text = item.Authors.First.Name.ToString() + "/ "
+                    .ForeColor = System.Drawing.Color.FromArgb(255, 255, 255)
+
+                    AddHandler User.Click, Sub(s As Object, e As EventArgs)
+                                               Process.Start(item.Links.First.Uri.ToString())
+                                           End Sub
+                End With
+
+                'Set Timelabel Properties
+                With Time
+                    .Top = 76
+                    .TextAlign = ContentAlignment.BottomRight
+                    .Size = New Size(750, 9)
+                    .Font = New System.Drawing.Font("Segoe UI", 6.0!)
+                    .Name = item.LastUpdatedTime.ToString()
+                    .Text = item.LastUpdatedTime.ToString().Substring(0, 10)
+                    .ForeColor = System.Drawing.Color.FromArgb(255, 255, 255)
+
+                    AddHandler Time.Click, Sub(s As Object, e As EventArgs)
+                                               Process.Start(item.Links.First.Uri.ToString())
+                                           End Sub
+                End With
+
+                ContentFlowPanel.Controls.Add(ContentPanel)
+                ContentPanel.Controls.Add(Title)
+                ContentPanel.Controls.Add(User)
+                ContentPanel.Controls.Add(Time)
+
+                If count = 1 Then
+                    If FirstTitle = String.Empty Then
+                        FirstTitle = Title.Text
+
+
+                    ElseIf String.Equals(FirstTitle, Title.Text) Then
+
+
+                        ' NotifyIcon1.ShowBalloonTip(1, "RssNotifyer", "No Changes", ToolTipIcon.Info)
+
+                    ElseIf Not String.Equals(FirstTitle, Title.Text) Then
+                        FirstTitle = Title.Text
+
+
+                        NotifyIcon1.ShowBalloonTip(1, "RssNotifyer - New Game", FirstTitle, ToolTipIcon.Info)
+                    End If
+
+
+
+                End If
+            Next
+            count = 0
+
+        Catch ex As Exception
+            MsgBox("No Reddit link inserted in Config.ini Or Server Down")
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub RssNotifyer_MouseWheel(sender As Object, e As MouseEventArgs) Handles Me.MouseWheel
+        If e.Delta > 0 Then
+
+            ContentFlowPanel.Top += 20
+        Else
+
+            ContentFlowPanel.Top -= 20
+        End If
+    End Sub
+
+    Private Sub ContentFlowPanel_MouseWheel(sender As Object, e As MouseEventArgs) Handles ContentFlowPanel.MouseWheel
+        If e.Delta > 0 Then
+
+            ContentFlowPanel.Top += 20
+        Else
+
+            ContentFlowPanel.Top -= 20
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ContentFlowPanel.Controls.Clear()
+        GetRss()
+    End Sub
+    Private Sub Launcher_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Visible = False
+            NotifyIcon1.Visible = True
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_Click(sender As Object, e As EventArgs) Handles NotifyIcon1.Click
+        Me.Visible = True
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Public Shared Function GetLauncherPath(path As String, count As Integer, substring As Integer) As String
+        Dim allLines As String() = File.ReadAllLines(path)
+        Dim randomLine As String = allLines(count)
+        Return randomLine.Substring(substring)
+    End Function
+
+    Public Sub SetLauncherPathSetting(path As String, count As Integer, textbox As String)
+
+
+        Dim lines() As String = System.IO.File.ReadAllLines(path)
+
+        lines(count) = textbox
+
+        System.IO.File.WriteAllLines(path, lines)
+    End Sub
+
+    Public Sub CheckStartUp()
+        If String.Equals(GetLauncherPath("Config.ini", 0, 8), "1") Then
+
+            My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath)
+            SetLauncherPathSetting("Config.ini", 0, "startup=null")
+        ElseIf String.Equals(GetLauncherPath("Config.ini", 0, 8), "0") Then
+            My.Computer.Registry.LocalMachine.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).DeleteValue(Application.ProductName)
+            SetLauncherPathSetting("Config.ini", 0, "startup=null")
+        End If
+
+
+
+
+
+    End Sub
+End Class
